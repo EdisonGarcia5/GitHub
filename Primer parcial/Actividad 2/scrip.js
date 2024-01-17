cbe9f9e9358ce3bfc9151950bb61cd0b2a290257
EdisonGarcía5 <edisongarcia542003@gmail.com>
2024-01-16 19:49:13 -0500

    .

--- Primer parcial/Actividad 2/scrip.js
+++ Primer parcial/Actividad 2/scrip.js
@@ -1,19 +1,22 @@
 
-function cargarDatos() {
-    fetch('tabla_comida.json')
-        .then(response => response.json())  
+function cargarDatos() { //cargar los datos y mostrarlos en la tabla.
+    fetch('tabla_comida.json') // obtener el contenido del archivo que se esta solicitando.
+        .then(response => response.json()) // convierte la respuesta a formato JSON.
         .then(data => {
-            const tabla = document.getElementById('tableComida');
-            data.forEach(element => {
-                let fila = tabla.insertRow();
-                let celdaAlimento = fila.insertCell();
-                let celdaProteina = fila.insertCell();  
 
+            // se crea una variables "tabla" y asigna un nombre "document".
+            const tabla = document.getElementById('tableComida');//la variable "tabla" al almacena el ID "tableComida" del html.
+            data.forEach(element => {
+                let fila = tabla.insertRow();//inserta filas de la tabla.
+                let celdaAlimento = fila.insertCell(); //agrega las celdas correspondientes del archivo JSON.
+                let celdaProteina = fila.insertCell();
+
+                //inserta los registros en las celdas del archivo JSON.
                 celdaAlimento.textContent = element.Alimento;
                 celdaProteina.textContent = element.Proteína;  
             });
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error:', error)); //si tiene errores lo muestra.
 }
 
-window.onload = cargarDatos;
+window.onload = cargarDatos; //se ejecutará cuando la ventana (página web) se haya cargado por completo.
